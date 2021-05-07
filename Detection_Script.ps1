534b31fc42a51fc8b90591a5a8f4407d169fdce4
Daniel Sjögren <Daniel.Sjogren@permobil.com>
2021-05-07 12:57:59 +0200

    Update Detection_Script.ps1

--- Detection_Script.ps1
+++ Detection_Script.ps1
@@ -17,13 +17,6 @@ If ($ProcessBit -eq "32-bit") {
     $InstalledApplications = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | select DisplayName, DisplayVersion
 }
 
-$ApplicationToSearchFor  = $InstalledApplications | Where {$_.DisplayName -eq 'TeamViewer'}
-If ($ApplicationToSearchFor)  {
-    If ([version]::Parse($ApplicationToSearchFor.DisplayVersion) -ge [version]::Parse('15.0.0.0') ) {
-        Write-Host "Installed"
-    }
-}
-
 $ApplicationToSearchFor  = $InstalledApplications | Where {$_.DisplayName -eq 'TeamViewer Host'}
 If ($ApplicationToSearchFor)  {
     If ([version]::Parse($ApplicationToSearchFor.DisplayVersion) -ge [version]::Parse('15.17.7.0') ) {
