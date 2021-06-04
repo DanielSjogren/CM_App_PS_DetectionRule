5e11e2b53a6c78ef94612bb22e79d8af0f622264
Daniel Sjögren <Daniel.Sjogren@permobil.com>
2021-06-04 11:14:17 +0200

    Update Detection_Script.ps1
    Added support for scenarios when the version contains letters

--- Detection_Script.ps1
+++ Detection_Script.ps1
@@ -17,11 +17,21 @@ If ($ProcessBit -eq "32-bit") {
     $InstalledApplications = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | select DisplayName, DisplayVersion
 }
 
+# $InstalledApplications | Sort-Object DisplayName
+
 $ApplicationToSearchFor  = $InstalledApplications | Where {$_.DisplayName -eq 'TeamViewer Host'}
 If ($ApplicationToSearchFor)  {
     ForEach ($App in $ApplicationToSearchFor) {
-        If ([version]::Parse($App.DisplayVersion) -ge [version]::Parse('15.17.7.0') ) {
-            Write-Host "Installed"
+        If ($App.DisplayVersion -match '[0-9.]$') {
+            # Version only contain dots and digits
+            If ([version]::Parse($App.DisplayVersion) -ge [version]::Parse('15.17.7.0') ) {
+                Write-Host "Installed"
+            }
+        } Else {
+            # Version doesn't only contain dots and digits
+            If ($App.DisplayVersion -eq '19.0a') {
+                Write-Host "Installed"
+            }
         }
     }
 }
