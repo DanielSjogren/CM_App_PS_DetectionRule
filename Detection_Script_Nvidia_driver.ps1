c1db6d78b2975234b0d00e0c4003a16cd2415509
Daniel Sjögren <Daniel.Sjogren@permobil.com>
2021-06-24 09:22:48 +0200

    Create Detection_Script_Nvidia_driver.ps1

---
+++ Detection_Script_Nvidia_driver.ps1
@@ -0,0 +1,7 @@
+﻿# Version taken from "Display.Driver\*.inf"
+$NvidiaDriver = $(Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.DeviceName -match "Nvidia Quadro" }).DriverVersion
+If ($NvidiaDriver) {
+    If ([version]::Parse($NvidiaDriver) -ge [version]::Parse('27.21.14.6259') ) {
+        Write-Host "Installed"
+    }
+}
